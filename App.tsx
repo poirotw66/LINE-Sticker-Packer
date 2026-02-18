c5a6ebdeadbec06f7f5e60feed75cbfedb277460
Chang Wei-Cyun <61868591+poirotw66@users.noreply.github.com>
2026-02-18 09:44:22 +0800

    refactor: Remove unused UUID import from App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 import { UploadedImage, StickerCount, AppState, Step } from './types';
 import { generateAndDownloadZip } from './services/zipService';
 import { ApiKeyProvider } from './contexts/ApiKeyContext';
